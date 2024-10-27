774da793e5a54b78dafccb2bf8a9f9afb887f33d
Starexify <nightbear843@yahoo.com>
2024-10-27 11:09:28 +0100

    Test

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,3 @@
-import CustomButton from './ui_elements/button.js';
+// import CustomButton from './ui_elements/button.js';
 
-window.customElements.define('custom-button', CustomButton);
+// window.customElements.define('custom-button', CustomButton);
